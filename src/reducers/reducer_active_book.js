09aeaadc05a0e53f338ddf35e3d45e33645bcfcd
Kedanco <kelvinchowjunjie120@hotmail.com>
2018-07-04 20:43:49 +0800

    added reducers and actions

---
+++ src/reducers/reducer_active_book.js
@@ -0,0 +1,8 @@
+// State argument is not application state, only the state this reducer is responsible for.
+export default function(state = null, action) {
+	switch (action.type) {
+		case "SELECT_BOOK":
+			return action.payload;
+	}
+	return state;
+}
